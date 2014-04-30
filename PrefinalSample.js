322f6a1768eb0430a2e597532d002df19cbc4171
Francis Baum <francis.j.baum@wustl.edu>
2014-04-30 16:36:05 -0500

    Create PrefinalSample.js
    Example geojson file to test with before final.

---
+++ PrefinalSample.js
@@ -0,0 +1,40 @@
+{ "type": "FeatureCollection",
+  "features": [ 
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-90.30445, 38.64908]},
+      "properties": {
+        "SiteID": "A-00007",
+        "Bldg": "095: Rudolph Hall" }
+    },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-90.3051, 38.64934]},
+      "properties": {
+        "SiteID": "A-00003",
+        "Bldg": "023: Compton Hall" }
+    },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-90.30526, 38.649]},
+      "properties": {
+        "SiteID": "A-00006",
+        "Bldg": "024: Crow Hall" }
+    },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-90.3032, 38.64908]},
+      "properties": {
+        "SiteID": "A-00002",
+        "Bldg": "125: Whitaker Hall" }
+    },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-90.30502, 38.64804]},
+      "properties": {
+        "SiteID": "A-00001",
+        "Bldg": "015: Brookings Hall" }
+    },
+    { "type": "Feature",
+      "geometry": {"type": "Point", "coordinates": [-90.30551, 38.64848]},
+      "properties": {
+        "SiteID": "A-00004",
+        "Bldg": "010: Beaumont Pavillion" }
+    }
+  ]
+}
